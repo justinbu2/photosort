83cd3e00006a3c26134459ccf2d34ebaee980b0f
Justin Bu <justinbu2@gmail.com>
2020-09-07 17:16:23 -0400

    support for custom date formats

--- photosort.py
+++ photosort.py
@@ -6,7 +6,7 @@ import shutil
 import sys
 from argparse import ArgumentParser
 from collections import Counter, defaultdict
-from datetime import datetime
+from datetime import date, datetime
 from time import ctime
 
 logger = logging.getLogger(__name__)
@@ -20,7 +20,8 @@ def parse_args():
     parser.add_argument("-r", "--rootdir", help="Root directory containing photos to be relocated", default=os.getcwd())
     parser.add_argument("-t", "--targetdir", help="Directory to which to relocate photos in --root", default=os.path.join(os.getcwd(), "_sorted"))
     parser.add_argument("-g", "--groupby", help="Attribute by which to group photos in a single folder", choices=SUPPORTED_GROUPINGS, default="year")
-    parser.add_argument("--rename", help="Specify whether to rename files to YYYY-MM-DD[_idx], preserving file extensions", action="store_true", default=False)
+    parser.add_argument("--rename", help="Specify whether to rename files to <date_fmt>[_idx], preserving file extensions", action="store_true", default=False)
+    parser.add_argument("-df", "--dateformat", help="Format of the date portion of resulting file names. Follows datetime.date.strftime convention", default="%Y%m%d")
     return parser.parse_args()
 
 def validate_filenames(rootdir):
@@ -37,7 +38,7 @@ def validate_filenames(rootdir):
         message += "\n".join(error_filepaths)
         raise Exception(message)
 
-def get_createdate_info(filename):
+def get_createdate(filename):
     """
     Returns the year, month, and day of the create date of
     `filename`.
@@ -48,6 +49,9 @@ def get_createdate_info(filename):
     create_date_ctime = ctime(create_date)
     _, month, day, _, year = create_date_ctime.split()
     datetime_obj = datetime.strptime(f"{year} {month} {day}", "%Y %b %d")
+    return datetime_obj
+
+def get_year_month_day(datetime_obj):
     year = str(datetime_obj.year)
     month = str(datetime_obj.month).rjust(2, "0")
     day = str(datetime_obj.day).rjust(2, "0")
@@ -59,7 +63,8 @@ def construct_groups(rootdir, groupby):
     groupsdict = defaultdict(list)
     for filename in os.listdir(rootdir):
         filepath = os.path.join(rootdir, filename)
-        year, month, day = get_createdate_info(filepath)
+        createdate = get_createdate(filepath)
+        year, month, day = get_year_month_day(createdate)
         if groupby == "year":
             groupname = year
         elif groupby == "month":
@@ -85,7 +90,7 @@ def copy_files(groupsdict, rootdir, targetdir):
             shutil.copyfile(filepath, targetpath)
             shutil.copystat(filepath, targetpath)
 
-def rename_files(targetdir):
+def rename_files(targetdir, datefmt):
     """
     Apple "Live" photos will have 1 jpeg and 1 mov for each photo. They
     share a file name, but have different extensions. Renamed
@@ -107,25 +112,25 @@ def rename_files(targetdir):
             # assign the indices here
             cur_filename = f"{filename_no_ext}.{file_exts[0]}"
             cur_filepath = os.path.join(groupdir, cur_filename)
-            createdate_info = get_createdate_info(cur_filepath)
-            date_counts[createdate_info] += 1
+            createdate = get_createdate(cur_filepath)
+            createdate_dateinfo = get_year_month_day(createdate)
+            date_counts[createdate_dateinfo] += 1
 
         # apply rename on filesystem
         seen_dates_counter = Counter()
         for filename_no_ext, file_exts in filename_exts.items():
             for i, file_ext in enumerate(file_exts):
                 filename = f"{filename_no_ext}.{file_ext}"
-                # rename each file to its create date
+                # rename each file to its formatted create date
                 cur_filepath = os.path.join(groupdir, filename)
-                createdate_info = get_createdate_info(cur_filepath)
-                year, month, day = createdate_info
-                create_date_str = "-".join([year, month, day])
-                new_filename = create_date_str
-                if date_counts[createdate_info] > 1:
+                createdate = get_createdate(cur_filepath)
+                new_filename = createdate.strftime(datefmt)
+                createdate_dateinfo = get_year_month_day(createdate)
+                if date_counts[createdate_dateinfo] > 1:
                     # we only append idx if there is >1 file with same create date
                     if i == 0:
-                        seen_dates_counter[createdate_info] += 1
-                    idx = seen_dates_counter[createdate_info]
+                        seen_dates_counter[createdate_dateinfo] += 1
+                    idx = seen_dates_counter[createdate_dateinfo]
                     new_filename = f"{new_filename}_{idx}"
                 file_ext = filename.split(".")[-1]
                 new_filename_w_ext = f"{new_filename}.{file_ext}"
@@ -151,8 +156,8 @@ def main():
 
     # renaming files
     if opts.rename:
-        logger.info("Renaming files to yyyy-mm-dd[_idx] format...")
-        rename_files(targetdir)
+        logger.info(f"Renaming files to {opts.dateformat}[_idx] format...")
+        rename_files(targetdir, opts.dateformat)
 
     logger.info("Completed successfully")
     return 0
