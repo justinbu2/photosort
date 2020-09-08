5a759fed939c6aa6175478dbde0e7be7535b8803
Justin Bu <justinbu2@gmail.com>
2020-09-07 23:46:53 -0400

    restructure to apply all file system changes at end

--- photosort.py
+++ photosort.py
@@ -14,6 +14,7 @@ logger = logging.getLogger(__name__)
 
 LOGGING_CONF_FILENAME = "logging.conf"
 SUPPORTED_GROUPINGS = {"year", "month", "date"}
+IGNORED_FILENAMES = {".DS_Store"}
 
 
 def parse_args():
@@ -39,18 +40,29 @@ def validate_filenames(rootdir):
         message += "\n".join(error_filepaths)
         raise Exception(message)
 
+def get_files_createdates(rootdir):
+    """
+    Return mapping of `rootdir`'s files to their respective create dates,
+    sorted by ascending create date. Ignores directories.
+    """
+    rootdir_filepaths = [
+        os.path.join(rootdir, f)
+        for f in os.listdir(rootdir)
+        if not os.path.isdir(f) and f not in IGNORED_FILENAMES]
+    filepath_createdate_pairs = [(filepath, get_createdate(filepath)) for filepath in rootdir_filepaths]
+    return OrderedDict(sorted(filepath_createdate_pairs, key=lambda elem: elem[1]))
+
 def get_createdate(filename):
-    """
-    Returns the year, month, and day of the create date of
-    `filename`.
-    N.B. Results are integer strings (month and day are padded)
-    """
+    epoch_createtime = get_epoch_createtime(filename)
+    _, month, day, time, year = epoch_createtime.split()
+    datetime_obj = datetime.strptime(f"{year} {month} {day} {time}", "%Y %b %d %H:%M:%S")
+    return datetime_obj
+
+def get_epoch_createtime(filename):
     stat = os.stat(filename)
     create_date = stat.st_birthtime
-    create_date_ctime = ctime(create_date)
-    _, month, day, _, year = create_date_ctime.split()
-    datetime_obj = datetime.strptime(f"{year} {month} {day}", "%Y %b %d")
-    return datetime_obj
+    epoch_createtime = ctime(create_date)
+    return epoch_createtime
 
 def get_year_month_day(datetime_obj):
     year = str(datetime_obj.year)
@@ -58,13 +70,12 @@ def get_year_month_day(datetime_obj):
     day = str(datetime_obj.day).rjust(2, "0")
     return year, month, day
 
-def construct_groups(rootdir, groupby):
-    # filenames are guaranteed to be distinct because
-    # we do not recursively search through rootdir
-    groupsdict = defaultdict(list)
-    for filename in os.listdir(rootdir):
-        filepath = os.path.join(rootdir, filename)
-        createdate = get_createdate(filepath)
+def construct_groups(files_createdates_dict, groupby):
+    """
+    Construct mapping of groupname -> [filepaths]
+    """
+    groups_dict = defaultdict(list)
+    for filepath, createdate in files_createdates_dict.items():
         year, month, day = get_year_month_day(createdate)
         if groupby == "year":
             groupname = year
@@ -74,91 +85,84 @@ def construct_groups(rootdir, groupby):
             groupname = f"{year}-{month}-{day}"
         else:
             raise Exception(f"Unsupported group: {groupby}")
-        groupsdict[groupname].append(filename)
-    return groupsdict
+        groups_dict[groupname].append(filepath)
+    return groups_dict
 
-def copy_files(groupsdict, rootdir, targetdir):
-    for groupname, groupfiles in groupsdict.items():
-        for filename in groupfiles:
-            # copy to group's folder
+def construct_copy_mapping(groups_dict, targetdir):
+    """
+    Construct mapping of root file path -> target file path,
+    based on groups assigned in `groups_dict`
+    """
+    copy_dict = {}
+    for groupname, group_filepaths in groups_dict.items():
+        for filepath in group_filepaths:
             targetgroup_dir = os.path.join(targetdir, groupname)
-            if not os.path.exists(targetgroup_dir):
-                os.makedirs(targetgroup_dir)
+            filename = filepath.split(os.path.sep)[-1]
             targetpath = os.path.join(targetgroup_dir, filename)
-            filepath = os.path.join(rootdir, filename)
-            if os.path.isdir(filepath):
-                continue
-            shutil.copyfile(filepath, targetpath)
-            shutil.copystat(filepath, targetpath)
+            copy_dict[filepath] = targetpath
+    return copy_dict
 
-def rename_files(targetdir, datefmt):
+def rename_copy_dict(copy_dict, files_createdates_dict, datefmt):
     """
-    Apple "Live" photos will have 1 jpeg and 1 mov for each photo. They
-    share a file name, but have different extensions. Renamed
-    files reflect the same naming convention.
+    @param copy_dict: mapping of {root_filepath: target_filepath}
+    @param files_createdates_dict: ordered mapping of {root_file_path: createdate}
+    @param datefmt: string format of the date to use in target filenames
+    @return: copy_dict, except with target file names renamed to formatted file dates
     """
-    for groupname in os.listdir(targetdir):
-        groupdir = os.path.join(targetdir, groupname)
+    # construct per-date file counts by file path
+    date_counts = Counter([get_year_month_day(cdate_obj) for cdate_obj in files_createdates_dict.values()])
 
-        # sort file iteration order by create datetime
-        groupdir_files = [os.path.join(groupdir, f) for f in os.listdir(groupdir)]
-        groupdir_files = sorted(groupdir_files, key=os.path.getmtime)
-        groupdir_files = [fpath.split(os.path.sep)[-1] for fpath in groupdir_files]
+    # construct dict of old name -> new name
+    # note that for Apple Live photos, the resulting file names in the target dir will be different
+    # eg. IMG_0001.jpeg -> targetdir/groupname/YYYYMMDD_1.jpeg
+    #     IMG_0001.mov  -> targetdir/groupname/YYYYMMDD_2.mov
+    renamed_copy_dict = {}
+    seen_dates_counter = Counter()
+    for filepath, createdate_obj in files_createdates_dict.items():
+        new_filename = createdate_obj.strftime(datefmt)
+        createdate_ymd = get_year_month_day(createdate_obj)
+        createdate_count = date_counts[createdate_ymd]
+        if createdate_count > 1:
+            # we only append idx if there is >1 file with same create date
+            seen_dates_counter[createdate_ymd] += 1
+            # pad index if >10 distinct files with same create date.
+            createdate_count_numdigits = int(math.log10(createdate_count)) + 1
+            idx = str(seen_dates_counter[createdate_ymd]).rjust(createdate_count_numdigits, "0")
+            new_filename = f"{new_filename}_{idx}"
+        file_ext = filepath.split(".")[-1]
+        new_filename = f"{new_filename}.{file_ext}"
+        target_filepath = copy_dict[filepath]
+        target_dir = os.path.sep.join(target_filepath.split(os.path.sep)[:-1])
+        new_filepath = os.path.join(target_dir, new_filename)
+        renamed_copy_dict[filepath] = new_filepath
+    return renamed_copy_dict
 
-        # construct mapping of filename -> extensions
-        filename_exts = OrderedDict()
-        for filename in os.listdir(groupdir):
-            file_ext = filename.split(".")[-1]
-            filename_no_ext = filename.replace(f".{file_ext}", "")
-            if filename_no_ext not in filename_exts:
-                filename_exts[filename_no_ext] = []
-            filename_exts[filename_no_ext].append(file_ext)
+def copy_files(copy_dict):
+    """
+    Copy files from paths specified in keys of dict to paths
+    specified in values of dict. Does not overwrite files in
+    target location if they already exist. Instead, we return
+    them as a list for caller to use.
+    """
+    pre_existing_files = []
+    for cur_filepath, target_filepath in copy_dict.items():
+        target_dir = os.path.dirname(target_filepath)
+        if not os.path.isdir(target_dir):
+            os.makedirs(target_dir)
+        if os.path.exists(target_filepath):
+            pre_existing_files.append((cur_filepath, target_filepath))
+            # do not overwrite file if target file already exists
+            continue
+        shutil.copyfile(cur_filepath, target_filepath)
+        shutil.copystat(cur_filepath, target_filepath)
+    return pre_existing_files
 
-        # construct per-date file counts by extension-agnostic file name
-        date_counts = Counter()
-        for filename_no_ext, file_exts in filename_exts.items():
-            # assign the indices here
-            cur_filename = f"{filename_no_ext}.{file_exts[0]}"
-            cur_filepath = os.path.join(groupdir, cur_filename)
-            createdate = get_createdate(cur_filepath)
-            createdate_dateinfo = get_year_month_day(createdate)
-            date_counts[createdate_dateinfo] += 1
+def roll_back(copy_dict, pre_existing_files):
+    for target_path in copy_dict.values():
+        if not os.path.exists(target_path) or target_path in pre_existing_files:
+            continue
+        os.remove(target_path)
 
-        # apply rename on filesystem
-        seen_dates_counter = Counter()
-        tmp_targetpaths = []
-        for filename_no_ext, file_exts in filename_exts.items():
-            for i, file_ext in enumerate(file_exts):
-                filename = f"{filename_no_ext}.{file_ext}"
-                # rename each file to its formatted create date
-                cur_filepath = os.path.join(groupdir, filename)
-                createdate = get_createdate(cur_filepath)
-                new_filename = createdate.strftime(datefmt)
-                createdate_dateinfo = get_year_month_day(createdate)
-                createdate_count = date_counts[createdate_dateinfo]
-                if createdate_count > 1:
-                    # we only append idx if there is >1 file with same create date
-                    if i == 0:
-                        seen_dates_counter[createdate_dateinfo] += 1
-                    createdate_count_numdigits = int(math.log10(createdate_count)) + 1
-                    idx = str(seen_dates_counter[createdate_dateinfo]).rjust(createdate_count_numdigits, "0")
-                    new_filename = f"{new_filename}_{idx}"
-                file_ext = filename.split(".")[-1]
-                new_filename_w_ext = f"{new_filename}.{file_ext}"
-
-                new_targetpath = os.path.join(groupdir, new_filename_w_ext)
-                new_tmp_targetpath = f"{new_targetpath}.tmp" # to prevent file loss, described below
-                os.rename(cur_filepath, new_tmp_targetpath)
-                tmp_targetpaths.append((new_tmp_targetpath, new_targetpath))
-
-        # we rename once more from the tmp target path to the actual target path
-        # this is to address potential file deletion issues with reshuffling
-        # of the file names
-        # eg. (DATE1_1.jpg -> DATE2.jpg, DATE2.jpg -> DATE1.jpg) will result in
-        # only 1 file
-        for targetpath_pair in tmp_targetpaths:
-            tmp_targetpath, act_targetpath = targetpath_pair
-            os.rename(tmp_targetpath, act_targetpath)
 
 def main():
     opts = parse_args()
@@ -168,18 +172,31 @@ def main():
     # validate file names
     validate_filenames(rootdir)
 
+    # dict of filepath -> create datetime object, sorted by create date
+    files_createdates_dict = get_files_createdates(rootdir)
+
     # construct groups
     logger.info("Constructing groups...")
-    groupsdict = construct_groups(rootdir, opts.groupby)
+    groups_dict = construct_groups(files_createdates_dict, opts.groupby)
 
-    # copy to group dirs
-    logger.info("Copying files to group folders...")
-    copy_files(groupsdict, rootdir, targetdir)
+    # construct dict of root paths to target paths
+    logger.info("Constructing copy mapping from rootdir to targetdir...")
+    copy_dict = construct_copy_mapping(groups_dict, targetdir)
 
     # renaming files
     if opts.rename:
-        logger.info(f"Renaming files to {opts.dateformat}[_idx] format...")
-        rename_files(targetdir, opts.dateformat)
+        logger.info(f"Updating copy mapping to rename files to {opts.dateformat}[_idx] format...")
+        copy_dict = rename_copy_dict(copy_dict, files_createdates_dict, opts.dateformat)
+
+    # apply the copy of files over to targetdir
+    logger.info(f"Copying files from {opts.rootdir} to {opts.targetdir}...")
+    pre_existing_files = copy_files(copy_dict)
+    if pre_existing_files:
+        fmtted_pre_existing_files = "\n".join([f"{pair[0]} -> {pair[1]}" for pair in pre_existing_files])
+        msg = f"Failed to write to following files. Rolling back all copied files.\n{fmtted_pre_existing_files}"
+        logger.error(msg)
+        roll_back(copy_dict, pre_existing_files)
+        return 1
 
     logger.info("Completed successfully")
     return 0
